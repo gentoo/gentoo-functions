f0ca05097fc2f454deae6eedc1bf94d6cd543013
Mike Gilbert <floppym@gentoo.org>
2020-11-19 13:20:18 -0500

    Update copyright notice
    Signed-off-by: Mike Gilbert <floppym@gentoo.org>

--- consoletype.c
+++ consoletype.c
@@ -3,7 +3,7 @@
  * simple app to figure out whether the current terminal
  * is serial, console (vt), or remote (pty).
  *
- * Copyright 1999-2007 Gentoo Foundation
+ * Copyright 1999-2020 Gentoo Authors
  * Distributed under the terms of the GNU General Public License v2
  */
 
