279ff36af3eada93776a5001f6125be12aea9241
Kerin Millar <kfm@plushkava.net>
2023-02-13 02:09:58 +0000

    Add global shellcheck hints
    Signed-off-by: Kerin Millar <kfm@plushkava.net>
    Signed-off-by: Sam James <sam@gentoo.org>

--- functions.sh
+++ functions.sh
@@ -1,5 +1,6 @@
 # Copyright 1999-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
+# shellcheck shell=sh disable=3043
 
 #
 # All functions in this file should be written in POSIX sh. Please do
