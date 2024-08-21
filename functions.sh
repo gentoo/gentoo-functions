gentoo/gentoo-functions

commit 5ee035a364bea8d12bc8abfe769014e230a212a6

Add the srandom() function
This is based on the behaviour of the special SRANDOM variable in bash.
It should be noted that sh is capable of bitwise arithmetic. For
instance, it is possible to clamp the number in such a way that it does
not exceed the minimum possible value of RAND_MAX (32767).

n=$(srandom) && : $(( n >>= 17 ))

Signed-off-by: Kerin Millar <kfm@plushkava.net>

functions.sh
@@ -12,6 +12,7 @@
 # The following variables affect initialisation and/or function behaviour.
 
 # BASH          : whether bash-specific features may be employed
+# BASH_VERSINFO : whether bash-specific features may be employed
 # BASHPID       : potentially used by _update_columns() to detect subshells
 # COLUMNS       : potentially used by _update_columns() to get the column count
 # EERROR_QUIET  : whether error printing functions should be silenced
@@ -471,6 +472,32 @@ vewend()
 	else
 		return "$1"
 	fi
+}
+
+#
+# Generates a random uint32 with the assistance of the kernel CSPRNG.
+#
+srandom()
+{
+	# shellcheck disable=3028
+	if [ "${BASH_VERSINFO:-0}" -ge 5 ]; then
+		srandom()
+		{
+			printf '%d\n' "${SRANDOM}"
+		}
+	elif [ -c /dev/urandom ]; then
+		srandom()
+		{
+			printf '%d\n' "0x$(
+				LC_ALL=C od -vAn -N4 -tx1 /dev/urandom | tr -d '[:space:]'
+			)"
+		}
+	else
+		warn "srandom: /dev/urandom doesn't exist as a character device"
+		return 1
+	fi
+
+	srandom
 }
 
 #

test-functions
@@ -397,6 +397,27 @@ test_yesno() {
 	iterate_tests 3 "$@"
 }
 
+test_srandom() {
+	set -- \
+		eq 0 \
+		eq 0 \
+		eq 0 \
+		eq 0 \
+		eq 0
+
+	row=0
+
+	callback() {
+		number=$(srandom)
+		test_description="srandom ($(( row += 1 ))/5: ${number:-blank})"
+		is_int "${number}" \
+		&& test "${number}" -ge 0 \
+		&& test "${number}" -le 4294967295
+	}
+
+	iterate_tests 2 "$@"
+}
+
 iterate_tests() {
 	slice_width=$1
 	shift
@@ -457,6 +478,7 @@ test_is_visible || rc=1
 test_yesno || rc=1
 test_die || rc=1
 test_edo || rc=1
+test_srandom || rc=1
 
 cleanup_tmpdir
 

commit d51d103cb345d3ba96af3c4b5bba9eeb13fe55f7

Check for SRANDOM support in a safer manner
Given that the SRANDOM variable loses its special properties if unset,
to compare two expansions of it to one another ought to be more robust.
Do so up to three times, so as not to be foiled by the unlikely event of
the RNG repeating the same number.

Further, the prior check was defective because it incorrectly presumed
the minimum required version of bash to be 5.0 rather than 5.1.

Fixes: 5ee035a364bea8d12bc8abfe769014e230a212a6
Signed-off-by: Kerin Millar <kfm@plushkava.net>

functions.sh
@@ -575,7 +575,20 @@ quote_args()
 srandom()
 {
 	# shellcheck disable=3028
-	if [ "${BASH_VERSINFO:-0}" -ge 5 ]; then
+	_has_srandom()
+	{
+		# The SRANDOM variable was introduced by bash 5.1. Check for at
+		# least 5.0, letting the alternate branch confirm its efficacy.
+		if [ "${BASH_VERSINFO-0}" -lt 5 ]; then
+			false
+		else
+			for _ in 1 2 3; do
+				test "${SRANDOM}" != "${SRANDOM}" && break
+			done
+		fi
+	}
+
+	if _has_srandom; then
 		srandom()
 		{
 			printf '%d\n' "$(( SRANDOM >> 1 ))"
@@ -625,6 +638,7 @@ srandom()
 		return 1
 	fi
 
+	unset -f _has_srandom
 	srandom
 }
 