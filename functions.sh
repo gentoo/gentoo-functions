gentoo/gentoo-functions

commit 8a83cf36a847fbd32990d3590bfd22a1516af898

Render hr() faster
Render hr() faster by eliminating the requirement to fork and execute
any external utilities after having established the intended length of
the rule. Also, use printf -v and string-replacing parameter expansion
where the shell is found to be bash. Doing so helps considerably because
bash is very slow at looping.

Signed-off-by: Kerin Millar <kfm@plushkava.net>

functions.sh
@@ -156,30 +156,36 @@ has_systemd()
 
 #
 # Prints a horizontal rule. If specified, the first parameter shall be taken as
-# a string to be repeated in the course of composing the rule. Otherwise, it
-# shall default to the <hyphen-minus>. If specified, the second parameter shall
-# define the length of the rule in characters. Otherwise, it shall default to
-# the width of the terminal if such can be determined, or 80 if it cannot be.
+# a string whose first character is to be repeated in the course of composing
+# the rule. Otherwise, or if specified as the empty string, it shall default to
+# the <hyphen-minus>. If specified, the second parameter shall define the length
+# of the rule in characters. Otherwise, it shall default to the width of the
+# terminal if such can be determined, or 80 if it cannot be.
 #
 hr()
 {
-	local length
+	local char hr i length
 
-	if is_int "$2"; then
+	if [ "$#" -ge 2 ] && is_int "$2"; then
 		length=$2
 	elif _update_tty_level <&1; [ "${genfun_tty}" -eq 2 ]; then
 		length=${genfun_cols}
 	else
 		length=80
 	fi
-	PATTERN=${1:--} awk -v "width=${length}" -f - <<-'EOF'
-	BEGIN {
-		while (length(rule) < width) {
-			rule = rule substr(ENVIRON["PATTERN"], 1, width - length(rule))
-		}
-		print rule
-	}
-	EOF
+	char=${1--}
+	char=${char%"${char#?}"}
+	if [ "${BASH}" ]; then
+		# shellcheck disable=3045
+		printf -v hr '%*s' "${length}" ''
+		eval 'printf %s\\n "${hr//?/"$char"}"'
+	else
+		i=0
+		while [ "$(( i += 1 ))" -le "${length}" ]; do
+			hr=${hr}${char}
+		done
+		printf '%s\n' "${hr}"
+	fi
 }
 
 #

test-functions
@@ -517,7 +517,7 @@ test_hr() {
 		eq  0  -----                          -    5    \
 		eq  0  ''                             xyz  0    \
 		eq  0  x                              xyz  1    \
-		eq  0  xyzxy                          xyz  5
+		eq  0  xxxxx                          xyz  5
 
 	callback() {
 		shift

commit f58674287f79ee537077cb17bad99ede0f3854c8

Initialise the hr variable in the hr() function
The hr() function composes a horizontal rule by appending characters to
the 'hr' variable, which is never initialised. One might think that it
matters not, given that the variable is declared as function-local.
Unfortunately, it does matter.

$ testcase='var=leaked; f() { local var; echo "var=${var-unset}"; }; f'
$ set -- bash 'busybox sh' ksh mksh yash dash
$ for sh; do printf '%s: ' "$sh"; $sh -c "$testcase"; done
bash: var=unset
busybox sh: var=unset
ksh: var=unset
mksh: var=unset
yash: var=unset
dash: var=leaked

It can be seen here that dash is an outlier. Such are the perils of
depending upon a feature whose behaviour is undefined by the Shell
Command Language specification. Address this issue by ensuring that the
'hr' variable is always initialised as the null string.

Fixes: 8a83cf36a847fbd32990d3590bfd22a1516af898
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

functions.sh
@@ -256,6 +256,7 @@ hr()
 	fi
 	c=${1--}
 	c=${c%"${c#?}"}
+	hr=
 	i=0
 	while [ "$(( i += 16 ))" -le "${length}" ]; do
 		hr=${hr}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}

test-functions
@@ -672,6 +672,7 @@ test_hr() {
 		expected=$1
 		shift
 		test_description="hr $(quote_args "$@")"
+		hr="leakage"
 		test "$(hr "$@")" = "${expected}"
 	}
 